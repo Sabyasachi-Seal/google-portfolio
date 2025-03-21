ac0cc0585f0eced2227d2bd9106b29ea0470accb
Sabyasachi-Seal <yoboy907@gmail.com>
2025-03-22 00:10:57 +0530

    Add build and SSG manifests; update logo image path syntax

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -18,7 +18,7 @@ export const Logo: React.FC<Props> = ({ height = 92, width = 272 }) => {
       width={width}
       alt="themed-logo"
       src={
-        theme === 'light' ? '/images/google.png' : '/images/google_light.png'
+        theme === 'light' ? `/images/google.png` : `/images/google_light.png`
       }
     />
   )
